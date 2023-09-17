9433b1273f376c368fbdc84345e07880cbd25c15
RogerAgu <bobjackson298@gmail.com>
2023-09-17 02:12:24 -0500

    magifying glass icon added

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -8,3 +8,10 @@ import MainSearch from "./MainSearch.vue";
     <h1> hello there</h1>
     </div>
 </template>
+
+<style scoped lang="postcss">
+ .wrapper{
+    @apply container mx-auto flex flex-col gap-4 py-4;
+ }
+
+</style>

50b15ba131a8992ff331907582f0797f99b1ce0a
RogerAgu <bobjackson298@gmail.com>
2023-09-21 20:51:28 -0500

    added router

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-import MainPage from '@/components/MainPage.vue'
+
 import NavBar from '@/components/NavBar.vue';
 </script>
 
 <template>
   <NavBar />
-  <MainPage />
+  <RouterView/>
 </template>
